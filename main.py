efcbab08f9396cbc03ab0ac4ae09ff28510bfa93
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2021-01-26 09:43:19 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+light.set_brightness(255)
 while True:
-    if input.sound_level() < 75 or input.light_level() < 10
+    print(input.sound_level())
+    if input.sound_level() <= 146 and input.light_level() < 10:
         music.ba_ding.play_until_done()
+        light.set_all(color.rgb(255,0,0))
+        pause(500)
+        music.ba_ding.play_until_done()
+        light.clear()
+        pause(500)
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+light.setBrightness(255)
+while (true) {
+    console.log(input.soundLevel())
+    if (input.soundLevel() <= 146 && input.lightLevel() < 10) {
+        music.baDing.playUntilDone()
+        light.setAll(color.rgb(255, 0, 0))
+        pause(500)
+        music.baDing.playUntilDone()
+        light.clear()
+        pause(500)
+    }
+    
+}
